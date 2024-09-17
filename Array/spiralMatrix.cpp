b2e4a44c6696ba4264cc2fb0d6ef6ed404400e19
Abubakar-40 <m.abubakar12378@gmail.com>
2024-09-17 12:02:24 +0500

    C++

--- Array/spiralMatrix.cpp
+++ Array/spiralMatrix.cpp
@@ -4,34 +4,36 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> v;
-        int total = matrix.size() * matrix[0].size();
+        int r = matrix.size();
+        int c = matrix[0].size();
+        int total = r * c;
         int count = 0;
-        for (int i = 0; i < matrix.size(); i++) {
+        for (int i = 0; i < r; i++) {
             if (count == total) {
                 break;
             }
-            for (int j = i; j < matrix[0].size() - i; j++) {
+            for (int j = i; j < c - i; j++) {
                 v.push_back(matrix[i][j]);
                 count++;
             }
             if (count == total) {
                 break;
             }
-            for (int k = i + 1; k < matrix.size() - i; k++) {
-                v.push_back(matrix[k][matrix[0].size() - 1 - i]);
+            for (int k = i + 1; k < r - i; k++) {
+                v.push_back(matrix[k][c - 1 - i]);
                 count++;
             }
             if (count == total) {
                 break;
             }
-            for (int l = matrix[0].size() - i - 2; l >= i; l--) {
-                v.push_back(matrix[matrix.size() - i - 1][l]);
+            for (int l = c - i - 2; l >= i; l--) {
+                v.push_back(matrix[r - i - 1][l]);
                 count++;
             }
             if (count == total) {
                 break;
             }
-            for (int m = matrix.size() - i - 2; m > i; m--) {
+            for (int m = r - i - 2; m > i; m--) {
                 v.push_back(matrix[m][i]);
                 count++;
             }
