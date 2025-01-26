cddc5cee7fd684cf303e2b6d9f551c4cf588560b
Abubakar-40 <m.abubakar12378@gmail.com>
2025-01-26 13:35:50 +0500

    C++

--- Linked List/sorting.cpp
+++ Linked List/sorting.cpp
@@ -27,3 +27,58 @@ public:
     }
 };
 
+// Approach # 2
+// Space Complexity : O(1)
+// Time Complexity : O(n log n)
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) { 
+        if(head == nullptr || head->next == nullptr){
+            return head;
+        }
+        return mergeSort(head); 
+    }
+
+    ListNode* middle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode *slow = head, *fast = head->next;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* mergeSort(ListNode* head) {
+
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* mid = middle(head);
+        ListNode* nextToMiddle = mid->next;
+        mid->next = nullptr;
+        ListNode* left = mergeSort(head);
+        ListNode* right = mergeSort(nextToMiddle);
+        return merge(left, right);
+    }
+    ListNode* merge(ListNode* left, ListNode* right) {
+        if (left == nullptr) {
+            return right;
+        }
+        if (right == nullptr) {
+            return left;
+        }
+        ListNode* result = nullptr;
+        if (left->val <= right->val) {
+            result = left;
+            result->next = merge(left->next, right);
+        } else {
+            result = right;
+            result->next = merge(left, right->next);
+        }
+        return result;
+    }
+};
+
