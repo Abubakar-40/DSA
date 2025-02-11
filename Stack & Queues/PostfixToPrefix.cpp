7ac4f769b81f083de9f13f2deb09cb68ffbff0cd
Muhammad Abubakar <m.abubakar12378@gmail.com>
2025-02-11 19:57:23 +0500

    GFG

---
+++ Stack & Queues/PostfixToPrefix.cpp
@@ -0,0 +1,37 @@
+//GFG : https://www.geeksforgeeks.org/problems/postfix-to-prefix-conversion/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=postfix-to-prefix-conversion
+
+// Space Complexity : O(N)
+// Time Complexity : O(N)
+
+class Solution {
+  public:
+    string postToPre(string post_exp) {
+        // Write your code here
+        string result;
+        stack<string> stk;
+        for(size_t i = 0; i<post_exp.size();i++){
+            char ch = post_exp[i];
+            if(ch == ' ')
+                continue;
+            else if(!isOperator(ch)){
+                stk.push(string(1,ch));
+            }
+            else{
+                string op2 = stk.top();
+                stk.pop();
+                string op1 = stk.top();
+                stk.pop();
+                
+                string temp = (string(1,ch)+op1+op2);
+                stk.push(temp);
+            }
+            
+        }
+        result = stk.top();
+        stk.pop();
+        return result;
+    }
+    bool isOperator(char c){
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+    }
+};
